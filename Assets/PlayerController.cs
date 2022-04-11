83da820fd7c5c64fa70a47bd1c05f93f2c0d1b64
Dylarno <medeirosdylan@hotmail.com>
2022-04-10 21:33:35 -0400

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool isMoving;
+
+    private Vector2 input;
+    void Update()
+    {
+        if (isMoving) return;
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        if (input == Vector2.zero) return;
+        if (input.x != 0) input.y = 0;
+
+        var targetPos = transform.position;
+
+        if (input.x != 0)
+        {
+            targetPos.x += input.x / 2f;
+            targetPos.y -= 0.25f * Mathf.Sign(input.x);
+        }
+
+        else if (input.y != 0)
+        {
+            targetPos.y += input.y / 4f;
+            targetPos.x += 0.5f * Mathf.Sign(input.y);
+        }
+
+        StartCoroutine(Move(targetPos));
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
