646e3d8127c81d41097fafae6f39d354e86e8062
Mark McGregor <markmcgregor.contact@gmail.com>
2022-04-12 06:33:12 -0400

    Add scene controller
    and fix player not dying when dying

---
+++ Assets/Managers/SceneManager/SceneController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; private set; }
+
+    private bool awaitingSceneChange;
+    private bool awaitingRetry;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        // setup event listeners
+        PlayerController.PlayerEscaped += OnPlayerEscaped;
+        PlayerController.PlayerDied += OnPlayerDied;
+    }
+
+    private void OnDestroy()
+    {
+        // clear event listeners
+        PlayerController.PlayerEscaped -= OnPlayerEscaped;
+        PlayerController.PlayerDied -= OnPlayerDied;
+    }
+
+    private void Update()
+    {
+        if (awaitingRetry && Input.GetKeyDown(KeyCode.Space))
+        {
+            awaitingSceneChange = false;
+            awaitingRetry = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            return;
+        }
+
+        if (awaitingSceneChange && Input.GetKeyDown(KeyCode.Space))
+        {
+            awaitingSceneChange = false;
+            awaitingRetry = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+    }
+
+    private void OnPlayerDied()
+    {
+        awaitingRetry = true;
+        awaitingSceneChange = false;
+    }
+
+    private void OnPlayerEscaped()
+    {
+        awaitingRetry = false;
+        awaitingSceneChange = true;
+    }
+}
